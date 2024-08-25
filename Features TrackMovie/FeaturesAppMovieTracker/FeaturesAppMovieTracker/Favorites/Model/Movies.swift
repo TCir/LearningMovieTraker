fb4ce1fe3b9b6c10a5c7c1e0d8b4509ff02f80c0
Thiago <thiagovalentim@hotmail.com.br>
2024-08-24 21:42:37 -0300

    cell configuration, creation of folders for MVVM architecture

---
+++ Features TrackMovie/FeaturesAppMovieTracker/FeaturesAppMovieTracker/Favorites/Model/Movies.swift
@@ -0,0 +1,27 @@
+//
+//  Movies.swift
+//  FeaturesAppMovieTracker
+//
+//  Created by Thiago Valentim on 22/08/24.
+//
+
+import Foundation
+
+
+struct Detail {
+    var title: String
+    var image: String
+}
+
+
+//struct Detail {
+//    var title: String
+//    var image: String
+//    var list: [Item]
+//}
+//
+//struct Item {
+//    var image: String
+//    var text: String
+//   
+//}
